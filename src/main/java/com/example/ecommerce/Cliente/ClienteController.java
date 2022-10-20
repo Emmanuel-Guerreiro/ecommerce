4560cfca51bbd4a8d86f5bbdfb0bd83e41e181c5
Cucchia <sruizcucchiarelli@gmail.com>
2022-10-19 21:29:31 -0300

    AvanzadoClienteController

--- src/main/java/com/example/ecommerce/Cliente/ClienteController.java
+++ src/main/java/com/example/ecommerce/Cliente/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.Cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,4 +17,28 @@ public class ClienteController {
     @Autowired
     ClienteController(ClienteService service){ this.service = service;}
 
+    @GetMapping("")
+    public ResponseEntity<?> getAll(){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente mas tarde.\"}");
+        }
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente mas tarde.\"}");
+        }
+    }
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Cliente entity){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.save(entity));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente mas tarde.\"}");
+        }
+    }
 }
