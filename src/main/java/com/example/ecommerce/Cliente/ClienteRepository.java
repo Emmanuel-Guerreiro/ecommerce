1f55c5cd8fab74f025b1a37949f9d3da72ee7314
Santiago Ruiz Cucchiarelli <sruizcucchiarelli@gmail.com>
2022-11-06 21:50:43 -0300

    modificado index

--- src/main/java/com/example/ecommerce/Cliente/ClienteRepository.java
+++ src/main/java/com/example/ecommerce/Cliente/ClienteRepository.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce.Cliente;
 
 import com.example.ecommerce.Base.BaseRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends BaseRepository<Cliente, Long> {
 }
--- src/main/java/com/example/ecommerce/Producto/ProductoController.java
+++ src/main/java/com/example/ecommerce/Producto/ProductoController.java
@@ -2,6 +2,9 @@ package com.example.ecommerce.Producto;
 
 import com.example.ecommerce.Base.BaseController;
 import com.example.ecommerce.Producto.DTO.DTOCreateProducto;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 public interface ProductoController extends BaseController<Producto, Long> {
 
